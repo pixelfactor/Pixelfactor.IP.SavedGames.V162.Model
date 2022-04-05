a70892a93860e7ba7f51453e2858a6effdeb4c22
George Daters <admin@pixelfactor.com>
2022-04-05 21:55:00 +0100

    v1.02 - Added component bay data

---
+++ Src/Pixelfactor.IP.SavedGames.V162.Model/ComponentBay.cs
@@ -0,0 +1,12 @@
+﻿namespace Pixelfactor.IP.SavedGames.V162.Model
+{
+    public class ComponentBay
+    {
+        /// <summary>
+        /// The id local to the ship this bay is placed on
+        /// </summary>
+        public int Id { get; set; }
+        public ComponentBayType Type { get; set; }
+        public string Name { get; set; }
+    }
+}
